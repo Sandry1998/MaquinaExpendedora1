d80cafac5c7c861469b4ee734b8cf0c7096f0f13
Sandry1998 <slopez@ceatformacion.com>
2025-05-06 18:14:33 +0200

    Maquina Expendedora 2.0

--- src/TipoDeCafe.java
+++ src/TipoDeCafe.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public enum TipoDeCafe {
+    Cafe_Con_Leche(1.20),
+    SOLO(1.00),
+    SOLO_AMERICANO(1.10),
+    CORTADO(1.10),
+    LECHE_MANCHADA(1.30),
+    CHOCOLATE(1.25),
+    TE_CON_LIMON(1.10),
+    CAPUCHINO(2.0);
+
+    private final double precio;
+
+    TipoDeCafe(double precio) {
+        this.precio = precio;
+    }
+    public double getPrecio() {
+        return precio;
+    }
 }
